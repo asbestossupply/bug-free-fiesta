45f2ae1bf57ddb779d0b49c7a4dd5e4f43c8231b
Noah Blumenthal <noah.blumenthal@gmail.com>
2022-12-22 15:27:41 -0500

    fix: error message when rate limited by all providers

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.post('/', async (req, res) => {
     res.status(200).json({ success: true, result: country });
   } catch (err) {
     console.log(err);
-    res.status(500).json({ success: false, error: 'error fetching, try again?'})
+    res.status(500).json({ success: false, error: err || 'error fetching, try again?'})
   }
 
 })
